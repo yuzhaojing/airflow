1c45bcf67719ee06d678821831dbad3d6a39016c
yuzhaojing <32435329+yuzhaojing@users.noreply.github.com>
2019-04-24 10:19:26 +0800

    Revert "修改hive_operator，改成hiveserver2方式连接hive，并兼容传入文件形式 (#63)"
    This reverts commit 975a31ce8b2cccec2ade4f1051cb256cfe50f417.

--- airflow/hooks/hive_hooks.py
+++ airflow/hooks/hive_hooks.py
@@ -842,20 +842,11 @@ class HiveServer2Hook(BaseHook):
         """
         results_iter = self._get_results(hql, schema,
                                          fetch_size=fetch_size, hive_conf=hive_conf)
+        header = next(results_iter)
         results = {
-            'data': '',
-            'header': ''
+            'data': list(results_iter),
+            'header': header
         }
-
-        try:
-            header = next(results_iter)
-            results = {
-                'data': list(results_iter),
-                'header': header
-            }
-        except StopIteration:
-            self.log.info("This hql returns no value.")
-
         return results
 
     def to_csv(
