fd86f32e6a403fbe6e6efab052dd4b6364980cea
Sheng, Li <OopsOutOfMemory@users.noreply.github.com>
2019-04-25 10:11:45 +0800

    Fix process (#67)
    * fix_process
    
    * refine
    
    * fix

--- airflow/models.py
+++ airflow/models.py
@@ -322,16 +322,99 @@ class DagBag(BaseDagBag, LoggingMixin):
         return self.dags.get(dag_id)
 
     def process_file(self, filepath, only_if_updated=True, safe_mode=True):
-        found_dags, mods, is_zipfile, file_last_changed_on_disk, mod_name = self.process_common(filepath, only_if_updated=True, safe_mode=True)
+        """
+        Given a path to a python module or zip file, this method imports
+        the module and look for dag objects within it.
+        """
+        found_dags = []
+
+        # if the source file no longer exists in the DB or in the filesystem,
+        # return an empty list
+        # todo: raise exception?
+        if filepath is None or not os.path.isfile(filepath):
+            return found_dags
 
         try:
-            sys.path.insert(0, filepath)
-            m = importlib.import_module(mod_name)
-            mods.append(m)
+            # This failed before in what may have been a git sync
+            # race condition
+            file_last_changed_on_disk = datetime.fromtimestamp(os.path.getmtime(filepath))
+            if only_if_updated \
+                and filepath in self.file_last_changed \
+                and file_last_changed_on_disk == self.file_last_changed[filepath]:
+                return found_dags
+
         except Exception as e:
-            self.log.exception("Failed to import: %s", filepath)
-            self.import_errors[filepath] = str(e)
-            self.file_last_changed[filepath] = file_last_changed_on_disk
+            self.log.exception(e)
+            return found_dags
+
+        mods = []
+        is_zipfile = zipfile.is_zipfile(filepath)
+        if not is_zipfile:
+            if safe_mode and os.path.isfile(filepath):
+                with open(filepath, 'rb') as f:
+                    content = f.read()
+                    if not all([s in content for s in (b'DAG', b'airflow')]):
+                        self.file_last_changed[filepath] = file_last_changed_on_disk
+                        # Don't want to spam user with skip messages
+                        if not self.has_logged:
+                            self.has_logged = True
+                            self.log.info(
+                                "File %s assumed to contain no DAGs. Skipping.",
+                                filepath)
+                        return found_dags
+
+            self.log.debug("Importing %s", filepath)
+            org_mod_name, _ = os.path.splitext(os.path.split(filepath)[-1])
+            mod_name = ('unusual_prefix_' +
+                        hashlib.sha1(filepath.encode('utf-8')).hexdigest() +
+                        '_' + org_mod_name)
+
+            if mod_name in sys.modules:
+                del sys.modules[mod_name]
+
+            with timeout(configuration.conf.getint('core', "DAGBAG_IMPORT_TIMEOUT")):
+                try:
+                    m = imp.load_source(mod_name, filepath)
+                    mods.append(m)
+                except Exception as e:
+                    self.log.exception("Failed to import: %s", filepath)
+                    self.import_errors[filepath] = str(e)
+                    self.file_last_changed[filepath] = file_last_changed_on_disk
+
+        else:
+            zip_file = zipfile.ZipFile(filepath)
+            for mod in zip_file.infolist():
+                head, _ = os.path.split(mod.filename)
+                mod_name, ext = os.path.splitext(mod.filename)
+                if not head and (ext == '.py' or ext == '.pyc'):
+                    if mod_name == '__init__':
+                        self.log.warning("Found __init__.%s at root of %s", ext, filepath)
+                    if safe_mode:
+                        with zip_file.open(mod.filename) as zf:
+                            self.log.debug("Reading %s from %s", mod.filename, filepath)
+                            content = zf.read()
+                            if not all([s in content for s in (b'DAG', b'airflow')]):
+                                self.file_last_changed[filepath] = (
+                                    file_last_changed_on_disk)
+                                # todo: create ignore list
+                                # Don't want to spam user with skip messages
+                                if not self.has_logged:
+                                    self.has_logged = True
+                                    self.log.info(
+                                        "File %s assumed to contain no DAGs. Skipping.",
+                                        filepath)
+
+                    if mod_name in sys.modules:
+                        del sys.modules[mod_name]
+
+                    try:
+                        sys.path.insert(0, filepath)
+                        m = importlib.import_module(mod_name)
+                        mods.append(m)
+                    except Exception as e:
+                        self.log.exception("Failed to import: %s", filepath)
+                        self.import_errors[filepath] = str(e)
+                        self.file_last_changed[filepath] = file_last_changed_on_disk
 
         for m in mods:
             for dag in list(m.__dict__.values()):
@@ -430,7 +513,6 @@ class DagBag(BaseDagBag, LoggingMixin):
                     self.file_last_changed[filepath] = file_last_changed_on_disk
                     if is_validate:
                         raise e
-
 
         else:
             zip_file = zipfile.ZipFile(filepath)
